00cffa673f5069a84e137f7168d7e0d0007192df
yama-chiiii <nagayama0409@gmail.com>
2023-12-05 02:18:54 +0900

    白い四角の追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ export default function Home() {
     const [count, setCount] = useState(0);
     return (
         <div className="flex justify-center items-center w-full h-screen bg-blue-50">
-            <div>React Counter</div>
-            <div>{count}</div>
+            <div className="bg-white h-4/6 w-80 rounded-2xl shadow-xl">
+                <div>React Counter</div>
+                <div>{count}</div>
 
-            <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+                <button onClick={() => setCount((prev) => prev + 1)}>+</button>
 
-            <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+                <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+            </div>
         </div>
     );
 }
