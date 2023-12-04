8f56999afb5009fe01b70551b73ac97ae313ea9d
yama-chiiii <nagayama0409@gmail.com>
2023-12-05 02:30:02 +0900

    文字の大きさと色と位置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     return (
         <div className="flex justify-center items-center w-full h-screen bg-blue-50">
             <div className="bg-white h-4/6 w-80 rounded-2xl shadow-xl">
-                <div>React Counter</div>
-                <div>{count}</div>
+                <div className="flex justify-center font-mono mt-12 text-3xl text-gray-400">React Counter</div>
+                <div className="flex justify-center mt-20 text-8xl text-blue-800">{count}</div>
 
                 <button onClick={() => setCount((prev) => prev + 1)}>+</button>
 
