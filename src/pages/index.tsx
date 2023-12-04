0a7e9d8508d6adbcb30dbd756b9874d783cf246d
yama-chiiii <nagayama0409@gmail.com>
2023-12-05 02:12:31 +0900

    背景色の追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export default function Home() {
     const [count, setCount] = useState(0);
     return (
-        <div>
+        <div className="flex justify-center items-center w-full h-screen bg-blue-50">
             <div>React Counter</div>
             <div>{count}</div>
 
