190fd0f4ecdc3e4571b29a9327580db4336e391e
yama-chiiii <nagayama0409@gmail.com>
2023-12-05 02:41:04 +0900

    プラスとマイナスボタンの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,18 @@ export default function Home() {
             <div className="bg-white h-4/6 w-80 rounded-2xl shadow-xl">
                 <div className="flex justify-center font-mono mt-12 text-3xl text-gray-400">React Counter</div>
                 <div className="flex justify-center mt-20 text-8xl text-blue-800">{count}</div>
-
-                <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-
-                <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+                <div className="flex justify-around mt-20">
+                    <div className="flex items-center justify-center w-16 h-16 bg-blue-800 text-white rounded-full">
+                        <button onClick={() => setCount((prev) => prev + 1)} className="w-12 h-12 mb-2 text-4xl">
+                            +
+                        </button>
+                    </div>
+                    <div className="flex items-center justify-center w-16 h-16 text-blue-800 rounded-full border-blue-800 border-2">
+                        <button onClick={() => setCount((prev) => prev - 1)} className="w-12 h-12 mb-2 text-5xl">
+                            -
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
